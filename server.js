d795ec50a4ed25e0fdb1b9720b1a83d97b0e2d4f
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-04-10 23:29:10 -0500

    create react app

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import routes from './routes'
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(routes);
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
